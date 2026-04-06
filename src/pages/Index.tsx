501f3be01b4777aa13b0f177ad05e722b15083da
Luana Silva <144965495+Lu-nas@users.noreply.github.com>
2026-04-06 01:50:36 -0300

    Update Index.tsx
    ajust nameApresentacao

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
-import PresentationVideo from "@/components/PresentationVideo";
+import Apresentação from "@/components/Apresentacao";
 import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -18,7 +18,7 @@ const Index = () => {
         <About />
         <Skills />
         <Projects />
-        <PresentationVideo/>
+        <Apresentacao/>
         <Experience />
         <Contact />
       </main>
